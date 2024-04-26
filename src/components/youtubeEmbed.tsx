2bd04a231b78818ec46031e155c069d4a915a675
John Doe <kozlarek.nicholas@gmail.com>
2024-04-26 14:17:44 -0600

    refactor: finish making responsive

--- src/components/youtubeEmbed.tsx
+++ src/components/youtubeEmbed.tsx
@@ -17,10 +17,10 @@ function YoutubeEmbed({ youtubeId, videoReset, selectedVideo }) {
           style={{ backgroundImage: `url(${selectedVideo.images.large})` }}
         >
           <div className="overlay-heading">
-            <h2>{selectedVideo.title}</h2>
+            <h2 className="fade-in-heading">{selectedVideo.title}</h2>
             <div className="row middle-on-small center-on-small" onClick={play}>
               <div className="column small-12 medium-6 large-4">
-                <a href="#!" className="c-button c-button--gooey">
+                <a className="c-button c-button--gooey">
                   play
                   <div className="c-button__blobs">
                     <div></div>
@@ -31,7 +31,9 @@ function YoutubeEmbed({ youtubeId, videoReset, selectedVideo }) {
               </div>
             </div>
           </div>
-          <p>{selectedVideo.description}</p>
+          <p className="fade-in-slide-down-paragraph">
+            {selectedVideo.description}
+          </p>
         </div>
       )}
       <iframe
